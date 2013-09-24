4a902f09111760c1f6f85b059866dfb04fb9fbf5
briandinh1 <briandinh1@gmail.com>
2013-09-23 23:16:25 -0700

    Github Upload
    took forever

---
+++ LabF3/src/labF3/LabF3.java
@@ -0,0 +1,51 @@
+package labF3;
+
+import java.util.Scanner;
+
+public class LabF3
+{
+
+        public static void main(String[] args)
+        {
+
+                String promptThirsty;
+                String promptBreakfast;
+                Scanner in = new Scanner(System.in);
+
+                System.out.printf("I am Hungry \n");
+
+                System.out.printf("Get in line \n");
+
+                System.out.printf("Buy lunch \n");
+
+                System.out.printf("Thirsty? Y/N Had breakfast y/n");
+                promptThirsty = in.nextLine();
+                promptBreakfast = in.nextLine();
+
+                if (promptThirsty.equals("Y") && promptBreakfast.equals("y")  )
+                {
+                        System.out.printf("Buy diet coke \n");
+                }
+
+                else if (promptThirsty.equals("N") && promptBreakfast.equals("n") )
+                {
+                        System.out.printf("Buy coke \n");
+                }
+
+                else
+                {
+                        System.out.printf("Buy water \n");
+                }
+
+
+                System.out.printf("Eat lunch \n");
+
+                System.out.printf("Return tray \n");
+
+                System.out.printf("Leave \n");
+
+
+
+        }
+
+}
